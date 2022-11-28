78b4f9490bc33b3a36807cc20d97aa5f9335a397
MhammadR <114111412+MhammadR@users.noreply.github.com>
2022-11-28 20:24:02 +0200

    Update NewTaskForm.js

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,25 @@
 import React from "react";
 
-function NewTaskForm() {
+function NewTaskForm({
+  categories,
+  newTask: { text, category },
+  onInputChange,
+  onTaskFormSubmit,
+}) {
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={onTaskFormSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input type="text" name="text" value={text} onChange={onInputChange} />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select name="category" value={category} onChange={onInputChange}>
+          {categories.map((category) =>
+            category === "All" ? null : (
+              <option key={category}>{category}</option>
+            )
+          )}
         </select>
       </label>
       <input type="submit" value="Add task" />
